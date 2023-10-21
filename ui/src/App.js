4a7c224e94a07e9105d30c86ba2c66603ac60083
kellerflint <kflint0068@gmail.com>
2023-10-21 00:06:52 -0700

    added markdown formatting

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 import axios from 'axios';
 import './App.css'
 
 function App() {
   const [query, setQuery] = useState('');
   const [response, setResponse] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const askQuestion = async () => {
+    setIsLoading(true);
     try {
       const res = await axios.post('http://127.0.0.1:5000/query', { query });
       setResponse(res.data.answer);
@@ -14,6 +17,7 @@ function App() {
       console.error('Error asking question:', error);
       setResponse('Sorry, an error occurred while trying to ask the question.');
     }
+    setIsLoading(false);
   };
 
   return (
@@ -24,8 +28,15 @@ function App() {
         onChange={(e) => setQuery(e.target.value)}
         placeholder="Ask a question..."
       />
-      <button onClick={askQuestion}>Ask</button>
-      {response && <div className="response"><strong>AI Response:</strong> {response}</div>}
+      <button onClick={askQuestion} disabled={isLoading}>
+        {isLoading ? 'Loading...' : 'Ask'}
+      </button>
+      {response &&
+        <div className="response">
+          <strong>AI Response:</strong>
+          <ReactMarkdown children={response} />
+        </div>
+      }
     </div>
   );
 }
